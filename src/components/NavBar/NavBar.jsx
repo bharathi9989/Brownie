9d0ed4614134d608c1230822f4acb570a37309a3
bharathi9989 <er.velubharathi.s@gmail.com>
2026-07-08 11:24:30 +0530

    update :navbar is updated

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,7 +5,7 @@ import React from "react";
 function NavBar() {
   return (
     <>
-      <nav className="flex items-center justify-between px-10 py-5">
+      <nav className="flex items-center justify-between p py-5 fixed top-0 left-0 w-full z-50 bg-transparent">
         {/* Logo */}
         <h1 className="text-2xl font-bold">Brownie</h1>
         {/* Navigation Link */}
